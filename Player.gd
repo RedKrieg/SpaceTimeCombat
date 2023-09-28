634baabade58ad4150a6865157208ac4982d9386
Brandon Whaley <redkrieg@gmail.com>
2023-09-28 16:53:29 -0400

    ignore build folder, first pass at keeping character sprite on screen

--- Player.gd
+++ Player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
+@onready var screensize = get_viewport_rect().size
+@onready var player_size = $ShipCollision.shape.get_rect().size
+
+
 var max_speed = 1000
-var input_scaler = 1500
+var input_scaler = 2000
 var friction_scaler = 0.95
+
+func _ready():
+	player_size = Vector2(player_size.y, player_size.x) # need to fix this when we get ship collision to not be stupid
 
 func _physics_process(delta):
 	var movement_vector = Input.get_vector("left", "right", "up", "down")
@@ -10,7 +17,9 @@ func _physics_process(delta):
 	velocity *= friction_scaler
 	if velocity.length() > max_speed:
 		velocity = velocity.normalized() * max_speed
+	position.x = clamp(position.x, player_size.x, screensize.x-player_size.x)
+	position.y = clamp(position.y, player_size.y, screensize.y-player_size.y)
 	if Input.is_action_just_pressed("fire_primary"):
 		# fire projectile
-		print("FIRE")
+		print(player_size)
 	move_and_slide()

a2af71f387b8febae0d24262e27e1d6cd108885c
Brandon Whaley <redkrieg@gmail.com>
2023-10-09 15:13:28 -0400

    add twin stick controls and mouse aiming

--- AssetScenes/star.gd
+++ AssetScenes/star.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 
-@export var twinkle_frequency := 2
+@export var twinkle_frequency := 2.0
 @export var twinkle_scale := 0.08
 var original_scale
 var twinkle_offset
@@ -15,6 +15,6 @@ func _ready():
 func _process(_delta):
 	var t = Time.get_ticks_msec() + twinkle_offset
 	# angle in radians that matches desired frequency
-	var s = t / 1000.0 * twinkle_frequency * PI
+	var s = t / 1000.0 * twinkle_frequency * 2 * PI
 	scale.x = original_scale.x * (1 - cos(s) * twinkle_scale)
 	scale.y = original_scale.y * (1 - sin(s) * twinkle_scale)
--- Player.gd
+++ Player.gd
@@ -13,9 +13,12 @@ extends CharacterBody2D
 @onready var player_half_size = get_bounds($ShipCollision)/2
 @onready var boost_value:float = 100.0
 @onready var boost_bar:ProgressBar = $"../UI/BoostContainer/BoostBar"
+@onready var cockpit_sprite:Sprite2D = $Cockpit
+@onready var has_touchscreen:bool = DisplayServer.is_touchscreen_available()
 
 # state machine vars
 @export var boosting:bool = false
+@export var firing:bool = false
 
 func _ready():
 	pass
@@ -23,24 +26,34 @@ func _ready():
 func _physics_process(delta):
 	# handle input
 	var movement_vector = Input.get_vector("left", "right", "up", "down")
+	var aim_vector = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+	var mouse_position = get_viewport().get_mouse_position()
+	# use both joystick and mouse to aim if not on touchscreen
+	if not has_touchscreen:
+		aim_vector += mouse_position - position
+	# update states
+	firing = Input.is_action_pressed("fire_primary")
+	boosting = (
+		Input.is_action_pressed("boost")
+		and
+		boost_value > boost_burn_per_second * delta
+		)
+	# handle firing
+	if firing:
+		print(mouse_position)
+	# handle boost
 	var acceleration = base_acceleration
 	var max_speed = base_max_speed
-	if Input.is_action_just_pressed("fire_primary"):
-		# fire projectile
-		print(boost_bar.value)
-	if Input.is_action_pressed("boost") and boost_value > boost_burn_per_second * delta:
-		boosting = true
+	if boosting:
 		acceleration = boost_acceleration
 		max_speed = boost_max_speed
 		boost_value -= boost_burn_per_second * delta
 	else:
-		boosting = false
 		# recharge boost
 		boost_value += boost_restore_per_second * delta
 	boost_value = clamp(boost_value, 0.0, 100.0)
 	# need to make this a signal later
 	boost_bar.value = boost_value
-	
 	# update velocity, apply friction first then acceleration
 	velocity = velocity.lerp(Vector2.ZERO, delta*friction_scaler)
 	velocity = velocity.lerp(movement_vector * acceleration, delta)
@@ -62,6 +75,7 @@ func _physics_process(delta):
 		position.y = new_position.y
 	# rotate ship to match travel direction
 	rotation = velocity.y / max_speed * rotation_angle_scaler
+	cockpit_sprite.rotation = aim_vector.angle()
 	# execute motion
 	move_and_slide()
 
